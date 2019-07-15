95c5b66c108159f8143197a7b148e2a6342bbcfa
1941 <550401736@qq.com>
2019-07-15 11:15:07 +0800

    request/response

--- flaskTest.py
+++ flaskTest.py
@@ -1,9 +1,9 @@
 #-*- encoding=UTF-8 -*-
 
-from flask import Flask
+from flask import Flask,render_template,request,make_response
 
 app=Flask(__name__)
-
+app.jinja_env.line_statement_prefix= '#'
 @app.route('/index/')
 @app.route('/')
 def index():
@@ -11,7 +11,22 @@ def index():
 
 @app.route('/profile/<uid>',methods=['Get','Post'])
 def profile(uid):
-    return 'profile:'+uid
+    colors =('red','green','black')
+    infos = {'i':'abc','j':'def'}
+    return render_template('profile.html',uid=uid,colors=colors,infos=infos)
+
+@app.route('/request')
+def request_demo():
+    key = request.args.get('key', 'defaultkey')
+    res=request.args.get('key','defaultkey')+'<br>'
+    res=res+request.url+'++'+request.path+'<br>'
+    for property in dir(request):
+        res=res+str(property)+'|==|'+str(eval('request.'+property))+'<br>'
+    response=make_response(res)
+    response.set_cookie('nwid',key)
+    response.status ='404'
+    response.headers['myface']='hello my face~'
+    return response
 
 
 if __name__ =='__main__':

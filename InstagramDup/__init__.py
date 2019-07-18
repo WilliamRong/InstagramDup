4cbad1d575a74addb8e6e11385a73382bca04d73
1941 <550401736@qq.com>
2019-07-18 21:44:45 +0800

    website established 2

--- InstagramDup/__init__.py
+++ InstagramDup/__init__.py
@@ -6,6 +6,7 @@
 from flask import Flask
 from flask_sqlalchemy import  SQLAlchemy
 app=Flask(__name__)#创建app
+app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 app.config.from_pyfile('app.conf')#从app.conf中初始化设置
 db=SQLAlchemy(app)#可以操作数据库了
 from InstagramDup import views,models
--- InstagramDup/views.py
+++ InstagramDup/views.py
@@ -4,10 +4,24 @@
 视图文件
 """
 from InstagramDup import app
-from flask import render_template
+from flask import render_template,redirect
 from models import Image,User
 from sqlalchemy import text
 @app.route('/')
 def index():
-    images=Image.query.order_by(text('id desc')).limit(10).all()#选10张图片传进模板
+    images=Image.query.order_by(text('id asc')).limit(10).all()#选10张图片传进模板
     return render_template('index.html',images=images)#渲染模板index.html
+
+@app.route('/image/<int:image_id>/')
+def image(image_id):
+    image=Image.query.get(image_id)
+    if image==None:
+        return redirect('/')
+    return render_template('pageDetail.html',image=image)
+
+@app.route('/profile/<int:user_id>/')
+def profile(user_id):
+    user=User.query.get(user_id)
+    if user==None:
+        return redirect('/')
+    return render_template('profile.html',user=user)
